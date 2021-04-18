7dd9be9f0e3afbc4c27ec9679e2dfcbf17d2c7f6
mcleaw <48169447+mcleaw@users.noreply.github.com>
2021-04-19 05:47:55 +0800

    first_commit

---
+++ lib/data/core/api_constraint.dart
@@ -0,0 +1,8 @@
+class ApiConstraints {
+  //agar private constructornya
+  ApiConstraints._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "b136e628d11dede14488d322e28071af";
+  static const String BASE_IMAGE_URL = " http://image.tmdb.org/t/p/w500/"
+}
